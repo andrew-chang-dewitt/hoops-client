71b967c09287028ba531ba54d76a1766085157a9
Andrew Chang-DeWitt <andrew@andrew-chang-dewitt.dev>
2023-03-09 13:30:05 -0600

    Still working on conditional rendering of children.

--- src/auth/redirect.rs
+++ src/auth/redirect.rs
@@ -22,45 +22,50 @@ pub fn AuthGuard(cx: Scope, children: Children) -> impl IntoView {
     let check_logged_in_action = create_server_action::<CheckLoggedIn>(cx);
     let checking_is_logged_in = create_resource(
         cx,
-        move || check_logged_in_action.version().get(),
+        move || (check_logged_in_action.version().get()),
         move |_| check_logged_in(cx),
     );
-    let is_logged_in = move || checking_is_logged_in.read(cx);
 
-    match is_logged_in() {
-        Some(Ok(maybe_user)) => {
-            if let Some(_) = maybe_user {
-                // render children if logged in
-                view! {
-                    cx,
-                    {children(cx)}
-                }
-                .into_view(cx)
-            } else {
-                // redirect to login page w/ "you have been logged out" error message if not
-                view! {
-                    cx,
-                    <LoggedOut msg=String::from( "you have been logged out" ) />
-                }
-                .into_view(cx)
-            }
-        }
-        // redirect to login page w/ "an error occurred, please log in" message on error
-        Some(Err(err)) => {
-            log::error!("Error checking if user is logged in: {err:#?}");
+    let check_result = move || checking_is_logged_in.read(cx).map(|n| n);
 
-            view! {
-                cx,
-                <LoggedOut msg=String::from( "an error occurred, please log in" ) />
-            }
-            .into_view(cx)
-        }
-        // resource fetcher still pending
-        _ => view! {
-            cx,
-            <p>"Loading..."</p>
-        }
-        .into_view(cx),
+    // TODO: this clearly won't work, maybe distill check_result into an option<bool> instead, then
+    // use a Show inside the suspense to optionally render the children?
+    // TODO: also add redirect logic using leptos_axum::navigate && leptos_router::use_navigate for
+    // ssr & not(ssr) contexts too in the Ok(None) & Err match arms
+    // see https://github.com/Indrazar/auth-sessions-example/blob/14817a048995a96ef1105abf502ad3e2b923b302/src/pages/components/redirect.rs#L41
+    // for example of navigation
+    view! {
+        cx,
+        <Suspense fallback={|| view!{cx, <>"Loading..."</>}}>
+            <>{
+                match check_result() {
+                    // User is logged in, render children
+                    Ok(Some(_)) => view! {
+                        cx,
+                        {children(cx)}
+                    }
+                    .into_view(cx),
+                    // User is not logged in, redirect to login page w/ "you have been logged out" error
+                    // message if not
+                    Ok(None) => view! {
+                        cx,
+                        <LoggedOut msg=String::from( "you have been logged out" ) />
+                    }
+                    .into_view(cx),
+                    // Error occurred while checking auth status, redirect to login page w/ "an error
+                    // occurred, please log in" message on error
+                    Err(err) => {
+                        log::error!("Error checking if user is logged in: {err:#?}");
+
+                        view! {
+                            cx,
+                            <LoggedOut msg=String::from( "an error occurred, please log in" ) />
+                        }
+                        .into_view(cx)
+                    }
+                }
+            }</>
+        </Suspense>
     }
 }
 
