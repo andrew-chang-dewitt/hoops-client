c6236371b49ef8b32992de66523e80fc44b4c46d
Andrew Chang-DeWitt <andrew@andrew-chang-dewitt.dev>
2023-03-09 16:32:14 -0600

    Also didn't work.

--- src/auth/redirect.rs
+++ src/auth/redirect.rs
@@ -26,45 +26,49 @@ pub fn AuthGuard(cx: Scope, children: Children) -> impl IntoView {
         move |_| check_logged_in(cx),
     );
 
-    let check_result = move || checking_is_logged_in.read(cx).map(|n| n);
+    let (err_msg, set_err_msg) = create_signal(cx, String::new());
+    let check_result = move || {
+        checking_is_logged_in
+            .read(cx)
+            .map(|n| -> bool {
+                match n {
+                    // User is logged in
+                    Ok(Some(_)) => true,
+                    // User is not logged in, redirect to login page w/ "you have been logged out" error
+                    // message if not
+                    Ok(None) => {
+                        set_err_msg(String::from("you have been logged out"));
+                        false
+                    }
+                    // Error occurred while checking auth status, redirect to login page w/ "an error
+                    // occurred, please log in" message on error
+                    Err(err) => {
+                        log::error!("Error checking if user is logged in: {err:#?}");
+                        set_err_msg(String::from("an error occurred, please log in"));
+                        false
+                    }
+                }
+            })
+            .unwrap_or(false)
+    };
 
     // TODO: this clearly won't work, maybe distill check_result into an option<bool> instead, then
     // use a Show inside the suspense to optionally render the children?
+    // TODO: FIXME: maybe not this ^^? Show feels wrong, it'll render LoggedOut when still pending
+    // instead of waiting to render...
     // TODO: also add redirect logic using leptos_axum::navigate && leptos_router::use_navigate for
     // ssr & not(ssr) contexts too in the Ok(None) & Err match arms
     // see https://github.com/Indrazar/auth-sessions-example/blob/14817a048995a96ef1105abf502ad3e2b923b302/src/pages/components/redirect.rs#L41
     // for example of navigation
     view! {
         cx,
-        <Suspense fallback={|| view!{cx, <>"Loading..."</>}}>
-            <>{
-                match check_result() {
-                    // User is logged in, render children
-                    Ok(Some(_)) => view! {
-                        cx,
-                        {children(cx)}
-                    }
-                    .into_view(cx),
-                    // User is not logged in, redirect to login page w/ "you have been logged out" error
-                    // message if not
-                    Ok(None) => view! {
-                        cx,
-                        <LoggedOut msg=String::from( "you have been logged out" ) />
-                    }
-                    .into_view(cx),
-                    // Error occurred while checking auth status, redirect to login page w/ "an error
-                    // occurred, please log in" message on error
-                    Err(err) => {
-                        log::error!("Error checking if user is logged in: {err:#?}");
-
-                        view! {
-                            cx,
-                            <LoggedOut msg=String::from( "an error occurred, please log in" ) />
-                        }
-                        .into_view(cx)
-                    }
-                }
-            }</>
+        <Suspense fallback={move || view!{cx, <>"Loading..."</>}}>
+            <Show
+                when={move || check_result()}
+                fallback={move |cx| view! { cx, <LoggedOut msg={err_msg()}/> }}
+            >
+                {children(cx)}
+            </Show>
         </Suspense>
     }
 }
